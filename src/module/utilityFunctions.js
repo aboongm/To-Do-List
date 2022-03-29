3bec8d1eb3a4f2d80f246a08bb4718ccc3f78db1
aboongm <aboongm@yahoo.com>
2022-03-29 19:00:50 +0530

    Add displayContent function on utilityFunctions.js

---
+++ src/module/utilityFunctions.js
@@ -0,0 +1,28 @@
+import Task from './Task';
+import * as Elements from './constElements';
+
+const displayContent = () => {
+  const taskList = document.querySelector('#taskList');
+  taskList.innerHTML = '';
+
+  Task.TaskObject.forEach((obj) => {
+    taskList.innerHTML += `
+      <div class="taskDynamic border-bottom  m-0 px-3 py-0 d-flex align-items-center justify-content-between" data-id=${obj.id}>
+              <div class="form-check mb-0 d-flex align-items-center justify-content-start">
+                <input class="checkBox form-check-input border" type="checkbox" value="" id="">
+                <label class="form-check-label p-3 m-0" for="">
+                  <span class="h5 m-0 p-0">${obj.description}</span>
+                </label>
+              </div>
+              <button class="three-dots h5 btn m-0 icon">
+                <i class="fa-solid fa-ellipsis-vertical"></i>
+              </button>
+              <button class="trash h5 btn m-0 icon hide">
+                <i class="fa-solid fa-trash-can"></i>
+              </button>
+            </div>
+      `;
+  });
+};
+
+export { displayContent };
