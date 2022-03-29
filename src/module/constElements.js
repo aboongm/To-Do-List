1fe6a2394e468bacca8fad54b5d13f268cc20a0b
aboongm <aboongm@yahoo.com>
2022-03-29 18:56:16 +0530

    Add the constElements modules and add const selectors

---
+++ src/module/constElements.js
@@ -0,0 +1,9 @@
+const refreshTask = document.querySelector('#refresh');
+const taskInput = document.querySelector('#task');
+const submitInput = document.querySelector('#arrow-submit');
+const clearCompletedTasks = document.querySelector('#clear-completed');
+
+const threeDots = document.querySelector('.three-dots');
+const trash = document.querySelector('.trash');
+
+export { taskInput, submitInput, refreshTask, clearCompletedTasks, threeDots, trash };
