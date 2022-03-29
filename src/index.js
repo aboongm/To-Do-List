add51b0c8f67206f4542ea060f0045a9ecd9253d
aboongm <aboongm@yahoo.com>
2022-03-29 15:38:02 +0530

    Remove the commented code  at line 18 in index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ const taskList = [
 
 const displayContent = () => {
   const tasks = document.querySelector('#taskList');
-  //   tasks.innerHTML = '';
   taskList.forEach((element) => {
     tasks.innerHTML += `
           <div class="border-bottom  m-0 px-3 py-0 d-flex align-items-center justify-content-between" data-id=${element.id}>
