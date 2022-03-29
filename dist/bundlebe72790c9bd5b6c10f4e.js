8627c749f4d582f6b4a45c9f6b132d8b807a0df1
aboongm <aboongm@yahoo.com>
2022-03-29 14:54:04 +0530

    Add corrections

---
+++ dist/bundlebe72790c9bd5b6c10f4e.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log(12345);
+
+/******/ })()
+;
+//# sourceMappingURL=bundlebe72790c9bd5b6c10f4e.js.map
