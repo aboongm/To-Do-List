cc53ebdef5c01d1907210eb5d930af03c45270d7
aboongm <aboongm@yahoo.com>
2022-03-29 14:11:52 +0530

    Add html main section that contains the to do task placeholder and input field, and bootstrap and fontawesome links

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles/style.css';
+import test from './module/test-01.js';
+
+console.log('Hello webpack');
+test();
---
+++ src/module/test-01.js
@@ -0,0 +1,5 @@
+function test() {
+  console.log('testing......');
+}
+
+export default test;
