268858a060e1772c6b040b5d2e6a2b3a2f979679
aboongm <aboongm@yahoo.com>
2022-03-31 16:31:49 +0530

    Remove the comment from constElements.js

--- src/module/constElements.js
+++ src/module/constElements.js
@@ -2,11 +2,8 @@ const refreshTask = document.querySelector('#refresh');
 const taskInput = document.querySelector('#task');
 const submitInput = document.querySelector('#arrow-submit');
 const taskList = document.querySelector('#taskList');
-// const clearCompletedTasks = document.querySelector('#clear-completed');
 
 const threeDots = document.querySelector('.three-dots');
 const trash = document.querySelector('.trash');
 
-export {
-  refreshTask, taskInput, submitInput, taskList, threeDots, trash,
-};
+export { refreshTask, taskInput, submitInput, taskList, threeDots, trash };
